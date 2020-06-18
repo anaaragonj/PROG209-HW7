ced2ecdf1ac73d080573639d58661938223296bb
ana georgina aragon juarez <ageorginaaragon@MacBook-Air-de-ana.local>
2020-06-17 23:25:56 -0700

    first commit

--- public/main.js
+++ public/main.js
@@ -1,5 +1,19 @@
+var lastID = 1;
+var trackerArray = [];
+var trackerObject= function ( night,nap, exercisename,reps,sets,weight, name, totalCalories) {
+    this.night = night;
+    this.nap = nap;
+    this.totalhrs = Number(night) + Number(nap);
+    this.exercisename = exercisename;
+    this.reps = reps;
+    this.sets = sets;
+    this.weight = weight;
+    this.name = name;
+    this.totalCalories = totalCalories;
+    this.ID = lastID++;
+};
 
-require(["FoodItem","exercise"],function(){
+require(["FoodItem","exercise","sleephrs"],function(){
     
     //creating breakfast food items
     let foodItems = [];
@@ -49,6 +63,82 @@ require(["FoodItem","exercise"],function(){
     //Add event when Clear Button for snack is clicked
     var clearBtnsnack = document.getElementById("clearButtonsnack");
     clearBtnsnack.addEventListener("click", buttonClearsnack, false);
+
+    //Submit 
+    var Submit = document.getElementById("submit");
+    Submit.addEventListener("click", submitdata, false);
+
+    //Submit
+    var submit1 = document.getElementById("submit1");
+    submit1.addEventListener("click", submitdata, false);
+
+    //Submit
+    var submit2 = document.getElementById("submit2");
+    submit2.addEventListener("click", submitdata, false);
+
+    
+    var updateBtn = document.getElementById("update");
+    updateBtn.addEventListener("click", updateList, false);
+
+function updateList(){
+    $.get("/getAlldata", function(data, status){
+
+        const whichElement = document.getElementById("trackerList")
+        trckerdata = data;
+        whichElement.innerHTML = "";
+        trckerdata.forEach(function(item, index) {   // build one li for each item in array
+            var li = document.createElement("li");
+            li.setAttribute("data-parm", item.ID);
+            li.setAttribute("class", "movieClass");
+            li.innerHTML =li.innerHTML +  " totalCalories: " + item.totalCalories; 
+            whichElement.appendChild(li);
+
+        });
+    });
+
+}
+
+function submitdata(){
+   
+        //var a =  document.getElementById("name").value;
+        //var b = document.getElementById("calories").value; 
+        var HowMany = document.getElementById("HowMany").value; 
+        //var d =  document.getElementById("namelunch").value; 
+        //var e=  document.getElementById("Calorieslunch").value; 
+        var HowManylunch=  document.getElementById("HowManylunch").value; 
+        //var g = document.getElementById("namedinner").value; 
+        //var h =  document.getElementById("caloriesdinner").value; 
+        var HowManydinner=  document.getElementById("HowManydinner").value; 
+        //var j=  document.getElementById("namesnack").value; 
+        //var k=  document.getElementById("Caloriessnack").value; 
+        var HowManysnack=  document.getElementById("HowManysnack").value; 
+        var exercisename=  document.getElementById("exercisename").value; 
+        var reps = document.getElementById("reps").value; 
+        var sets =  document.getElementById("sets").value; 
+        var weight=  document.getElementById("weight").value; 
+        var night=  document.getElementById("night").value; 
+        var nap=  document.getElementById("nap").value; 
+        var totalCalories = HowMany + HowManylunch + HowManysnack + HowManydinner;
+        
+        
+        newData = new trackerObject( night,nap, exercisename,reps,sets,weight, name, totalCalories);
+
+        $.ajax({
+         url : "/AddData",
+         type: "POST",
+         data: JSON.stringify(newData),
+         contentType: "application/json; charset=utf-8",
+         dataType   : "json",
+         success: function (result) {
+             console.log(result);
+             document.location.href = "index.html#Show";  // go to this page to show item was added
+         }
+        });
+        
+
+} 
+
+
 
     //Breakfast - This function clears the screen 
     function buttonClear () {
@@ -156,6 +246,7 @@ require(["FoodItem","exercise"],function(){
         foodItemssnack.push(newFoodItem);
         return;
     }
+});
 
     // Fitness page
         
@@ -197,7 +288,111 @@ require(["FoodItem","exercise"],function(){
         return;
      }
 
+     // Sleep hours page
+    //creating Sleep HRS
+    let sleephrsarray = [];
+    let totalhrs = 0;
 
-     
+    //Add event when Click to add to your total button is clicked
+    var addsleephrs = document.getElementById("addsleephrs");
+    addsleephrs.addEventListener("click", buttonClicked3, false);
+
+    //Add event when Clear Button is clicked
+    var clearBtnsleephrs = document.getElementById("clearBtnSH");
+    clearBtnsleephrs.addEventListener("click", buttonclearsleephrs, false);
+
+    function buttonclearsleephrs () {
+        document.getElementById ("night").value = "";
+        document.getElementById ("nap").value = "";
+    }
+
+    // This function adds the sleep hrs to the array 
+    function buttonClicked3() {
+        myFunctionSleephrs();
+        outputText = "";
+        for (i = 0; i < sleephrsarray.length; i++) {
+            outputText += sleephrsarray[i].night + ", " + sleephrsarray[i].nap;
+            totalhrs +=  sleephrsarray[i].totalhrs; 
+        }  
+        document.getElementById("outputsh1").innerHTML = outputText;
+        document.getElementById("outputsh2").innerHTML = totalhrs;
+    }
+     //This function creates the total of hrs
+     function myFunctionSleephrs() {  
+        let night = document.getElementById ("night").value;
+        let nap = document.getElementById ("nap").value;
+        let newsleephrs = new sleephrs(night, nap);
+        sleephrsarray.push(newsleephrs);
+        return;
+     }
+
+     // this function is shared by Home and Delete page to add li's to which ever ul is passed in
+function UpdateDisplay() {
+
+    $.get("/getAlldata", function(data, status){  // AJAX get
+        trackerArray = data;  // put the returned server json data into our local array
     
-});
+    whichElement = document.getElementById("trackerList")
+
+    console.log(trackerArray);
+
+    whichElement.innerHTML = "";
+    trackerArray.forEach(function(item, index) {   // build one li for each item in array
+        var li = document.createElement("li");
+        li.setAttribute("data-parm", item.ID);
+        //li.setAttribute("class", "movieClass");
+        //whichElement.appendChild(li);
+
+        // I got rid of this "double link" approach that I picked up from a web example years ago
+        //li.innerHTML =li.innerHTML +  ": " + " ID: " + item.ID + "  " + item.Title +"  <a  data-transition='pop'  class='oneMovie'  data-parm=" + item.ID + " href='#home'> Get Details </a> "
+        
+        li.innerHTML =li.innerHTML +  " : " + " ID: " + item.ID + "  "; 
+       
+        // var uriTitle = encodeURI(item.title);  // this is needed if you are using a field that might have spaces in it.  we do not
+     
+      // now each li contains text for ID and title, and also an embedded data-parm value and class name
+    });   // end of forEach array 
+  
+     // now we loop thru the li's and add an event for each one 
+     // if user clicks any, it writes >>that<< items data-parm into the hidden html 
+     var classname = document.getElementsByClassName("movieClass");  // using css class to mark a collection of elements, or li's
+     Array.from(classname).forEach(function (element) {
+         element.addEventListener('click', function(){
+             var parm = this.getAttribute("data-parm");  // passing in the ID of a particular movie
+             document.getElementById("IDparmHere").innerHTML = parm;  // this element must be on the page with the li's
+             document.location.href = "index.html#details";  // jump to details page
+         });
+    });  // end of adding li events
+
+
+})
+}
+     //var data = {
+       // name: "Ana",
+        //totalhrs:"8",
+        //totalCaldinner:"500",
+        //totalCallunch:"500",
+        //totalCalories:"500",
+        //totalCalsnack:"300",
+        //exercise:"45"
+        
+     // }
+  
+  //var jsonData = JSON.stringify(data);
+
+  //var fs = require('fs');
+//fs.writeFile("lifestyle.JSON", jsonData, function(err) {
+  //  if (err) {
+    //    console.log(err);
+    //}
+//});
+
+//var fs = require("fs");
+
+//fileManager  = {
+  //read: function() {
+    //var rawdata = fs.readFileSync('lifestyle.json');
+    //let goodData = JSON.parse(rawdata);
+    //ServerArray = goodData;
+    //},
+     
--- routes/index.js
+++ routes/index.js
@@ -8,3 +8,127 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+var fs = require("fs");
+
+fs.appendFile("./lifestyle.json","", function(err){});
+
+fileManager  = {
+  read: function() {
+    var rawdata = fs.readFileSync('./lifestyle.json');
+    let goodData = JSON.parse(rawdata);
+    ServerArray = goodData;
+    },
+  
+
+  write: function() {
+    let data = JSON.stringify(ServerArray);
+    fs.writeFileSync('./lifestyle.json', data);
+  },
+
+  validData: function() {
+  var rawdata = fs.readFileSync('./lifestyle.json');
+  console.log(rawdata.length);
+  if(rawdata.length < 1) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+};
+
+var lastID = 1;
+var ServerArray = [];
+var trackerObject= function ( night, exercisename,reps,sets,weight, name, Calories, HowMany) {
+    this.night = night;
+    this.nap = nap;
+    this.totalhrs = Number(night) + Number(nap);
+    this.exercisename = exercisename;
+    this.reps = reps;
+    this.sets = sets;
+    this.weight = weight;
+    this.name = name;
+    this.Calories = Calories;
+    this.HowMany = HowMany;
+    this.totalCalories = Calories * HowMany;
+    this.ID = lastID++;
+   
+  }
+  
+
+if(!fileManager.validData()) {
+  ///ServerArray.push(new MovieObject("MoonstruckXXX", 1981, "Drama", "Nicholas Cage", "Cher", "https://www.youtube.com/watch?v=M01_2CKL6PU" ));
+  ///ServerArray.push(new MovieObject("Wild At Heart", 1982, "Drama", "Nicholas Cage", "Laura VanDern", "https://www.youtube.com/watch?v=7uRJartX79Q"));
+  //ServerArray.push(new MovieObject("Raising Arizona", 1983, "Comedy", "Nicholas Cage", "Holly Hunter", "https://www.youtube.com/watch?v=NoXJKArYi1g"));
+  //ServerArray.push(new MovieObject("USS Indianapolis: Men of Courage" , 2016, "Drama", "Nicholas Cage", "Emily Tennant", "https://youtu.be/ZDPE-NronKk"));
+  //fileManager.write();
+  //console.log("added 4 movies");
+}
+ else{
+  lastID = 0;
+  fileManager.read(); // do have prior info so load up the array
+  for (let i=0; i < ServerArray.length; i++){
+    if(Number(ServerArray[i].ID) >= Number(lastID) ){
+      console.log(ServerArray[i].ID + "  " + lastID);
+     lastID = (ServerArray[i].ID) +1;
+  }
+}
+ 
+}
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('index' );
+});
+
+/* GET home page. */
+router.get('/getAlldata', function(req, res, next) {
+  fileManager.read();
+  res.json(ServerArray);
+});
+
+
+
+
+//  new code ***********************************************
+
+/* Add one new note */
+router.post('/AddData', function(req, res) {
+  const newInfo = req.body;
+  newInfo.ID = lastID++;
+  ServerArray.push(newInfo);
+  fileManager.write();
+  res.status(200).json(newInfo);
+});
+
+
+router.delete('/DeleteNote/:ID', (req, res) => {
+  const delID = req.params.ID;
+  let found = false;
+
+  for(var i = 0; i < ServerArray.length; i++) // find the match
+  {
+    console.log(ServerArray[i].ID)
+      if(ServerArray[i].ID == delID){
+        ServerArray.splice(i,1);  // remove object from array
+        fileManager.write();
+          found = true;
+          break;
+      }
+  }
+
+  if (!found) {
+    console.log("not found");
+    return res.status(500).json({
+      status: "error: no such index in array"
+    });
+  } else {
+  res.send('Info with ID: ' + delID + ' deleted!');
+  }
+});
+
+module.exports = router;
